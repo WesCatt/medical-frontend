f86e60b9d017251f3d1d272c41a446bf5394d6f9
wesCatt <zzxcmdyx@163.com>
2025-08-08 14:15:21 +0800

    feat: 实现url保护性

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NODE_ENV === 'production' ? "http://medical-backend.westcat.cn/api" : "http://127.0.0.1:2587/api";
+const baseURL = import.meta.env.MODE === 'production' ? import.meta.env.VITE_PRODUCTION_URL : import.meta.env.VITE_DEV_URL;
 
 const api = axios.create({
     baseURL,
